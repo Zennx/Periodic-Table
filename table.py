efd75dfcaf5762ad859ce5f68d6ea7f465d7465a
Zennx <43442326+Zennx@users.noreply.github.com>
2018-10-18 23:18:13 +0800

    Initial commit

---
+++ table.py
@@ -0,0 +1,214 @@
+import os
+
+def clr(): #this is for resetting screen
+    input ("Press enter to return.")
+    os.system("cls")
+
+def d1(): #elements
+    print ("This is Hydrogen.")
+
+def d2(): 
+    print ("This is Helium")
+
+def d3(): 
+    print ("This is Lithium")
+
+def d4():
+    print("")
+
+def d5():
+    print ("")
+
+def d6():
+    print ("")
+
+def d7():
+    print ("")
+
+def d8():
+    print ("")
+
+def d9():
+    print ("")
+
+def d10():
+    print ("")
+
+def d11():
+    print ("")
+
+def d12():
+    print ("")
+
+def d13():
+    print ("")
+
+def d14():
+    print("")
+
+def d15():
+    print ("")
+
+def d16():
+    print ("")
+
+def d17():
+    print ("")
+
+def d18():
+    print ("")
+
+def d19():
+    print ("")
+
+def d20():
+    print ("")
+
+def d21():
+    print ("")
+
+def d22():
+    print ("")
+
+def d23():
+    print ("")
+
+def d24():
+    print ("")
+
+def d25():
+    print ("")
+
+def d26():
+    print ("")
+
+def d27():
+    print ("")
+
+def d28():
+    print ("")
+
+def d29():
+    print ("")
+
+def d30():
+    print ("")
+
+def d31():
+    print ("")
+
+def d32():
+    print ("")
+
+def d33():
+    print ("")
+
+def d34():
+    print ("")
+
+def d35():
+    print ("")
+
+def d36():
+    print ("")
+
+def d37():
+    print ("")
+
+def d38():
+    print ("")
+
+def d39():
+    print ("")
+
+def d40():
+    print ("")
+
+def d41():
+    print ("")
+
+def d42():
+    print ("")
+
+def d43():
+    print ("")
+
+def d44():
+    print ("")
+
+def d45():
+    print ("")
+
+def d46():
+    print ("")
+
+def d47():
+    print ("")
+
+def d48():
+    print ("")
+
+def tracker() : #main nav
+    number = input("Enter a number")
+    if number == "1" :
+        d1()
+        tracker()
+    elif number == "2" :
+        d2()
+        tracker()
+    elif number == "3" :
+        d3()
+        tracker()
+    elif number == "4":
+        d4()
+        tracker()
+    elif number == "5":
+        d5()
+        tracker()
+    elif number == "6":
+        d6()
+        tracker()
+    elif number =="7":
+        d7()
+        tracker()
+    elif number == "8":
+        d8()
+        tracker()
+    elif number == "9":
+        d9()
+        tracker()
+    elif number == "10":
+        d10()
+        tracker()
+    elif number == "11":
+        d11()
+        tracker()
+    elif number == "12":
+        d12()
+        tracker()
+    elif number == "13":
+        d13()
+        tracker()
+    elif number == "14":
+        d14()
+        tracker()
+    elif number == "15":
+        d15()
+        tracker()
+    elif number == "16":
+        d16()
+        tracker()
+    elif number == "17":
+        d17()
+        tracker()
+    elif number == "18":
+        d18()
+        tracker()
+    elif number == "19":
+        d19()
+        tracker()
+    exit()
+
+
+#game -lol under construction
+tracker()
